8578cb804bdb9cb6dd731dec985631e36abd3341
Jeisson Gomez <jeypitbullz4@gmail.com>
2025-08-12 17:11:13 -0500

    Actualizacion del proyecto

---
+++ Tipos/1. Variables.py
@@ -0,0 +1,12 @@
+nombre_curso = "Ultimate Pyton"
+Nombre1 = "hola"
+NOMBRE_CURSO="Python para principiantes"
+NoMbReCuRsO = "Chanchito"
+NombreCurso = "Feliz"
+
+print(nombre_curso, Nombre1, NOMBRE_CURSO)
+
+alumnos = 5000
+puntaje = 9.9
+publicado = True
+cursos = ["Python", "Django", "Flask"]

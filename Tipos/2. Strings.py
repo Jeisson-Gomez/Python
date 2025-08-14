97823f21fbdd45cae7c64206e76d33d175a4adf5
Jeisson Gomez <jeypitbullz4@gmail.com>
2025-08-13 19:48:43 -0500

    Actualizacion del proyacto

---
+++ Tipos/2. Strings.py
@@ -0,0 +1,8 @@
+Nombre_Curso = "Python desde cero"
+descipcion_curso = '''
+Este curso de Python desde cero es un curso introductorio donde aprenderás los conceptos básicos de Python.
+'''
+
+print(Nombre_Curso, descipcion_curso)
+print(len(Nombre_Curso) )
+print(Nombre_Curso[0:6]) 

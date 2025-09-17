480aa4a4695c7ca62148f0af92c0fa9e9981ea84
Jeisson Gomez <jeypitbullz4@gmail.com>
2025-09-17 15:41:34 -0500

    Actualizacion del proyecto

--- Tipos/4. Metodos-Strings.py
+++ Tipos/4. Metodos-Strings.py
@@ -1,6 +1,11 @@
-Animal = "Chanchito feliz"
+Animal = " Chanchito feliz "
 
 print(Animal.capitalize())
+print(Animal.find("cH"))
 print(Animal.lower())
+print(Animal.strip())
+print(Animal.strip().capitalize())
+print(Animal.lstrip())
+print(Animal.rstrip())
 print(Animal.title())
 print(Animal.upper())

96b6fe99a344da09db58988fcac641f68c9d2662
Jeisson Gomez <jeypitbullz4@gmail.com>
2025-10-27 20:04:45 -0500

    Actualizacion Del proyecto

---
+++ Tipos/6. Numeros.py
@@ -0,0 +1,18 @@
+Numero = 2 #Entero -> Integer
+Decimal = 1.2 # Float
+Imaginario = 2 + 2j # 2 + 2i
+
+# Numero = Numero+ 2
+Numero += 2
+Numero *= 5
+Numero -= 5
+Numero /= 5
+
+print("numero" , Numero)
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+print(1 // 3)
+print(8 % 3)
+print(2 ** 3)

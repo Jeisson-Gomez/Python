6983cf4369b425325e7e0ff42e266461111084fd
Jeisson Gomez <jeypitbullz4@gmail.com>
2025-11-23 13:43:08 -0500

    Actulizaicon del proyecto

--- Control Flujo/2.If.py
+++ Control Flujo/2.If.py
@@ -1,10 +1,12 @@
-Edad = 60
-if Edad > 54:
+Edad = 70
+if Edad > 60:
+    print("Puedes ver la pelicula Con super descuento")
+elif Edad > 54:
     print("Puede ver la pelicula con descuento")
 elif Edad > 17:
-    print("Puedes ver la pelicula")
-else:
-    print("No puedes entrar")
-print("Ve a otro lado")
+    print("Puede ver la pelicula")
+# else:
+#    print("No puedes entrar")
+#    print("Ve a otro lado")
 
 print("Listo")

5c70f7c95df75565cc65da0abb8cd12ff2f487c4
Jeisson Gomez <jeypitbullz4@gmail.com>
2026-02-03 22:36:36 -0500

    Actualizacion del proyecto

--- Control Flujo/6.For.py
+++ Control Flujo/6.For.py
@@ -1,8 +1,11 @@
-Buscar = 3
+Buscar = 10
 
 for Numero in range(5):
     print(Numero)
-    
+
     if Numero == Buscar:
         print("Encontrado", Buscar)
         break
+
+else:
+    print("No encontre el numero buscado :(")

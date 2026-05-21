c3834eae4243e9ab550e54fc36ed1fe0f5a21cfb
Jeisson Gomez <jeypitbullz4@gmail.com>
2026-05-21 15:37:06 -0500

    Actualizacion del proyecto

--- 2.Control Flujo/07.while.py
+++ 2.Control Flujo/07.while.py
@@ -1,4 +1,10 @@
-Numero = 1
-while Numero < 100:
-    print(Numero)
-    Numero *= 2
+#*Numero = 1
+#while Numero < 100:
+#    print(Numero)
+#    Numero *= 2
+
+Comando = ""
+
+while Comando != "salir":
+    Comando = input("$ ")
+    print(Comando)

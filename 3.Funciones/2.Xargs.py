38cc331bda187c704da598e61b51bf7a0ff86ee3
Jeisson Gomez <jeypitbullz4@gmail.com>
2026-06-19 11:48:10 -0500

    Actualizacion del proyecto

--- 3.Funciones/2.Xargs.py
+++ 3.Funciones/2.Xargs.py
@@ -1,5 +1,9 @@
 def Suma(*Numeros):
-    print(a + b + c)
+    Resultado = 0
+    for Numero in Numeros:
+        Resultado += Numero
+    print(Resultado)
 
 Suma(2, 5, 3)
 Suma(2, 5, 3, 4)
+Suma(2, 8, 7, 45, 32)
